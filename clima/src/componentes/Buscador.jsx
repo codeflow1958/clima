913ac2696f4300314564224da141a0005e8f49d5
nicky <nickylds1958@gmail.com>
2023-10-22 01:20:10 -0600

    g

--- clima/src/componentes/Buscador.jsx
+++ clima/src/componentes/Buscador.jsx
@@ -1,8 +1,27 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL } from "./api";
 
 const Buscador = ({ onSearchChange }) => {
   const [search, setSearch] = useState(null);
+
+  const loadOptions = (inputValue) => {
+    return fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${inputValue}&limit=5&appid=${GEO_API_URL}`
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        return {
+          option: response.data.map((city) => {
+            return {
+              value: `${city.lat} ${city.lon}`,
+              lable: `${city.name} ${city.country}`,
+            };
+          }),
+        };
+      })
+      .catch((err) => console.log(err));
+  };
 
   const handleOnchange = (searchData) => {
     setSearch(searchData);
@@ -15,8 +34,18 @@ const Buscador = ({ onSearchChange }) => {
       debouceTimeout={600}
       value={search}
       onChange={handleOnchange}
+      loadOptions={loadOptions}
     />
   );
 };
 
 export default Buscador;
+
+/* return {
+          option: response.data.map((city) => {
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              lable: `${city.name} ${city.countryCode} `,
+            };
+          }),
+        };*/
---
+++ clima/src/componentes/api.jsx
@@ -0,0 +1,4 @@
+
+  
+
+export const GEO_API_URL = "2f9b41a511d1351d341bc7bd79cd2e13"
