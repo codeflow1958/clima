e3bd4104cfec49cc1ad5f88ece4259c4f7920788
nicky <nickylds1958@gmail.com>
2023-10-22 22:36:18 -0600

    d

--- clima/src/componentes/Buscador.jsx
+++ clima/src/componentes/Buscador.jsx
@@ -4,24 +4,43 @@ import { GEO_API_URL } from "./api";
 
 const Buscador = ({ onSearchChange }) => {
   const [search, setSearch] = useState(null);
+   const [data, setData] = useState(null);
+   const getData = async (url, setState) => {
+     const res = await fetch(url);
+     const datos = await res.json();
 
-  const loadOptions = (inputValue) => {
+     setState(datos);
+   };
+
+   //// aca esta el use efect
+   useEffect(() => {
+     getData(
+       `http://api.openweathermap.org/geo/1.0/direct?q=${inputValue}&limit=5&appid=${API_KEY}`,
+       setData
+     );
+   }, []);
+
+   const loadOptions = (inputValue) => { 
+
+   }
+
+  /*const loadOptions = (inputValue) => {
     return fetch(
       `http://api.openweathermap.org/geo/1.0/direct?q=${inputValue}&limit=5&appid=${GEO_API_URL}`
     )
       .then((response) => response.json())
       .then((response) => {
         return {
-          option: response.data.map((city) => {
+          option: response.data.map((el) => {
             return {
-              value: `${city.lat} ${city.lon}`,
+              value: `${el.lat} ${city.lon}`,
               lable: `${city.name} ${city.country}`,
             };
           }),
         };
       })
       .catch((err) => console.log(err));
-  };
+  };*/
 
   const handleOnchange = (searchData) => {
     setSearch(searchData);
